2cccd023882881170d11d1fc68744bc2a754a0fe
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-10-02 16:42:34 +0900

    initial commit

---
+++ lib/App/Personal/Kuniyoshi/Rename.pm
@@ -0,0 +1,32 @@
+package App::Personal::Kuniyoshi::Rename;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Personal::Kuniyoshi::Rename -
+
+=head1 SYNOPSIS
+
+  use App::Personal::Kuniyoshi::Rename;
+
+=head1 DESCRIPTION
+
+App::Personal::Kuniyoshi::Rename is
+
+=head1 AUTHOR
+
+kuniyoshi E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
